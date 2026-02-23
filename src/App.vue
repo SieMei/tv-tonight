93362b2125e03a6aeb799b9d026d84bf24ee3209
sietse <sietsemeijer01@gmail.com>
2026-02-23 21:30:26 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="dummytext">
+    <h1>This will be the home page for TV Tonight</h1>
+    <p>It will display a list of TV shows and their details.</p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style>
+.dummytext {
+  font-family: sans-serif;
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
